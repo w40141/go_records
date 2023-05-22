2a2652d05b354a4f8df0746909d847b850a1f97d
w40141 <w.40141@gmail.com>
2023-05-22 14:09:41 +0900

    add goweb

---
+++ goTour/hello.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+
+func main()  {
+  fmt.Println("Hello world")
+	fmt.Println("My favorite number is", rand.Intn(10))
+  func foo() {
+    foo :=
+        "bar"
+}
+}
